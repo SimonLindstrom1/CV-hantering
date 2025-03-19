b5a8266162e6ca1cb2c4350bd84c37fea3df53b8
Simon Lindström <simon.tiger.lindstrom@gmail.com>
2025-03-19 15:32:18 +0100

    Filled in User and WorkExperience

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,17 @@
-﻿namespace CV_hantering_REST_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CV_hantering_REST_API.Models
 {
     public class User
     {
+        [Key]
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required string Description { get; set; }
+        public virtual List<Education>? Educations { get; set; }
+        public virtual List<WorkExperience>? WorkExperiences { get; set; }
     }
 }
--- Models/WorkExperience.cs
+++ Models/WorkExperience.cs
@@ -1,6 +1,19 @@
-﻿namespace CV_hantering_REST_API.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CV_hantering_REST_API.Models
 {
     public class WorkExperience
     {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("User")]
+        public required int UserIdFk { get; set; }
+        public required string JobTitle { get; set; }
+        public required string Employer { get; set; }
+        public string? Description { get; set; }
+        public required DateOnly StartDate { get; set; }
+        public DateOnly? EndDate { get; set; }
+
     }
 }
