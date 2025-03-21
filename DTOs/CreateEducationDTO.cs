ac84bc46ff37c8b3aa596def052f27799dfe2ae6
Simon Lindström <simon.tiger.lindstrom@gmail.com>
2025-03-21 14:31:11 +0100

    updated education dtos

--- DTOs/CreateEducationDTO.cs
+++ DTOs/CreateEducationDTO.cs
@@ -1,6 +1,18 @@
-﻿namespace CV_hantering_REST_API.DTOs
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CV_hantering_REST_API.DTOs
 {
     public class CreateEducationDTO
     {
+        public required string School { get; set; }
+        public string? Degree { get; set; }
+
+        public string? FieldOfStudy { get; set; }
+        public required DateOnly StartDate { get; set; }
+        public DateOnly? EndDate { get; set; }
+        
+        
+        [ForeignKey("User")]
+        public int? UserIdFK { get; set; }
     }
 }
